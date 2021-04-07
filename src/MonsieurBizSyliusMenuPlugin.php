47fe1303212924bc41fb03f141d1f4e80121ad1f
clemence pereira <c.pereira@monsieurbiz.com>
2021-04-07 13:33:29 +0200

    Init plugin

---
+++ src/MonsieurBizSyliusMenuPlugin.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Menu plugin for Sylius.
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusMenuPlugin;
+
+use LogicException;
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class MonsieurBizSyliusMenuPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+
+    /**
+     * Returns the plugin's container extension.
+     *
+     * @throws LogicException
+     *
+     * @return ExtensionInterface|null The container extension
+     */
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->containerExtension) {
+            $this->containerExtension = false;
+            $extension = $this->createContainerExtension();
+            if (null !== $extension) {
+                $this->containerExtension = $extension;
+            }
+        }
+
+        return $this->containerExtension instanceof ExtensionInterface
+            ? $this->containerExtension
+            : null;
+    }
+}
