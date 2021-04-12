626102576abfd765d758669d2d154c74fb41ed84
clemence pereira <c.pereira@monsieurbiz.com>
2021-04-12 13:40:55 +0200

    Add repository and type

---
+++ src/Form/Type/MenuItemType.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Menu plugin for Sylius.
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusMenuPlugin\Form\Type;
+
+use MonsieurBiz\SyliusMenuPlugin\Entity\MenuItem;
+use Doctrine\ORM\EntityRepository;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Sylius\Bundle\ResourceBundle\Form\Type\ResourceTranslationsType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+final class MenuItemType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event): void {
+                $item = $event->getData();
+                $event->getForm()
+                    ->add('parent', EntityType::class, [
+                        'class' => MenuItem::class,
+                        'required' => false,
+                        'choice_label' => 'translation.label',
+                        'choice_value' => 'id',
+                        'query_builder' => function(EntityRepository $er) use ($item) {
+                            $qb = $er->createQueryBuilder('o');
+                            $qb
+                                ->where('o.menu = :menu')
+                                ->setParameter('menu', $item->getMenu())
+                            ;
+                            if (null !== $item && null !== $item->getId()) {
+                                $qb
+                                    ->andWhere('o != :currentItem')
+                                    ->setParameter('currentItem', $item)
+                                ;
+                            }
+
+                            return $qb;
+                        },
+                    ])
+                    ->add('highlighted', CheckboxType::class, [
+                        'label' => 'monsieurbiz_menu.ui.highlighted',
+                        'required' => true,
+                    ])
+                    ->add('translations', ResourceTranslationsType::class, [
+                        'label' => 'sylius.ui.translations',
+                        'entry_type' => MenuItemTranslationType::class,
+                    ])
+                ;
+            })
+        ;
+    }
+}
