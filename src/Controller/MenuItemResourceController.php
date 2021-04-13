250bcdb07e0403df3413cee1a71dd1b42c034db8
clemence pereira <c.pereira@monsieurbiz.com>
2021-04-13 09:06:59 +0200

    Change header comment

--- src/Controller/MenuItemResourceController.php
+++ src/Controller/MenuItemResourceController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of Monsieur Biz' Menu plugin for Sylius.
+ * This file is part of Monsieur Biz' menu plugin for Sylius.
  * (c) Monsieur Biz <sylius@monsieurbiz.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,8 +11,8 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusMenuPlugin\Controller;
 
-use MonsieurBiz\SyliusMenuPlugin\Entity\MenuItemInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use MonsieurBiz\SyliusMenuPlugin\Entity\MenuItemInterface;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Sylius\Component\Resource\ResourceActions;
 use Symfony\Component\HttpFoundation\Request;
