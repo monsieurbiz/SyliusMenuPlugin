e0ea389fbb57f66f4ef3c580f8abc6600e946d2d
clemence pereira <c.pereira@monsieurbiz.com>
2021-04-13 11:59:40 +0200

    Create menu fixture

---
+++ src/Fixture/MenuFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' menu plugin for Sylius.
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusMenuPlugin\Fixture;
+
+use Doctrine\ORM\EntityManagerInterface;
+use MonsieurBiz\SyliusMenuPlugin\Fixture\Factory\MenuFixtureFactoryInterface;
+use Sylius\Bundle\CoreBundle\Fixture\AbstractResourceFixture;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+final class MenuFixture extends AbstractResourceFixture
+{
+    public function __construct(EntityManagerInterface $menuManager, MenuFixtureFactoryInterface $exampleFactory)
+    {
+        parent::__construct($menuManager, $exampleFactory);
+    }
+
+    public function configureResourceNode(ArrayNodeDefinition $resourceNode): void
+    {
+        /** @phpstan-ignore-next-line */
+        $resourceNode
+            ->children()
+                ->booleanNode('isSystem')->end()
+                ->scalarNode('createdAt')->defaultNull()->end()
+                ->scalarNode('updatedAt')->defaultNull()->end()
+                ->scalarNode('code')->cannotBeEmpty()->end()
+            ->end()
+            ;
+    }
+
+    public function getName(): string
+    {
+        return 'monsieurbiz_menu';
+    }
+}
