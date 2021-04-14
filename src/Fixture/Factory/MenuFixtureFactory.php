82633832a2185f609a644afca80602fe016a374a
clemence pereira <c.pereira@monsieurbiz.com>
2021-04-14 17:48:23 +0200

    Add menuItem fixtures

--- src/Fixture/Factory/MenuFixtureFactory.php
+++ src/Fixture/Factory/MenuFixtureFactory.php
@@ -12,6 +12,11 @@ declare(strict_types=1);
 namespace MonsieurBiz\SyliusMenuPlugin\Fixture\Factory;
 
 use MonsieurBiz\SyliusMenuPlugin\Entity\MenuInterface;
+use MonsieurBiz\SyliusMenuPlugin\Entity\MenuItem;
+use MonsieurBiz\SyliusMenuPlugin\Entity\MenuItemInterface;
+use MonsieurBiz\SyliusMenuPlugin\Entity\MenuItemTranslationInterface;
+use MonsieurBiz\SyliusMenuPlugin\Factory\MenuItemFactory;
+use MonsieurBiz\SyliusMenuPlugin\Repository\MenuItemRepository;
 use Sylius\Bundle\CoreBundle\Fixture\Factory\AbstractExampleFactory;
 use Sylius\Component\Product\Generator\SlugGeneratorInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
@@ -20,15 +25,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MenuFixtureFactory extends AbstractExampleFactory implements MenuFixtureFactoryInterface
 {
-    /**
-     * @var FactoryInterface
-     */
-    private $menuFactory;
 
-    /**
-     * @var OptionsResolver
-     */
-    private $optionResolver;
+    private FactoryInterface $menuFactory;
+
+    private FactoryInterface $menuItemFactory;
+    
+    private FactoryInterface $menuItemTranslationFactory;
+
+    private OptionsResolver $optionResolver;
 
     /**
      * @var \Faker\Generator
@@ -40,25 +44,50 @@ class MenuFixtureFactory extends AbstractExampleFactory implements MenuFixtureFa
      */
     private $slugGenerator;
 
-    public function __construct(FactoryInterface $menuFactory, SlugGeneratorInterface $slugGenerator)
+    /**
+     * @var MenuItemRepository
+     */
+    private $menuItemRepository;
+
+    public function __construct(FactoryInterface $menuFactory, FactoryInterface $menuItemFactory, FactoryInterface $menuItemTranslationFactory, SlugGeneratorInterface $slugGenerator, MenuItemRepository $menuItemRepository)
     {
         $this->menuFactory = $menuFactory;
+        $this->menuItemFactory = $menuItemFactory;
         $this->slugGenerator = $slugGenerator;
+        $this->menuItemTranslationFactory = $menuItemTranslationFactory;
         $this->optionResolver = new OptionsResolver();
         $this->configureOptions($this->optionResolver);
         $this->faker = \Faker\Factory::create();
+        $this->menuItemRepository = $menuItemRepository;
     }
 
     public function create(array $options = []): MenuInterface
     {
         $options = $this->optionResolver->resolve($options);
-
         /** @var MenuInterface $menu */
         $menu = $this->menuFactory->createNew();
         $menu->setIsSystem($options['isSystem']);
-        $menu->setCreatedAt($options['createdAt']);
-        $menu->setUpdatedAt($options['updatedAt']);
         $menu->setCode($options['code']);
+        $position = 1;
+
+        foreach ($options['items'] as $item) {
+            /** @var MenuItemInterface $menuItem */
+            $menuItem = $this->menuItemFactory->createNew();
+            $menuItem->setHighlighted($item['highlighted']);
+            $menuItem->setPosition($position++);
+
+            foreach ($item['translations'] as $locale => $translation)
+            {
+                /** @var MenuItemTranslationInterface $menuItemTranslation */
+                $menuItemTranslation = $this->menuItemTranslationFactory->createNew();
+                $menuItemTranslation->setLabel($translation['label']);
+                $menuItemTranslation->setLocale($locale);
+
+                $menuItem->addTranslation($menuItemTranslation);
+            }
+
+            $menu->addItem($menuItem);
+        }
 
         return $menu;
     }
@@ -69,15 +98,10 @@ class MenuFixtureFactory extends AbstractExampleFactory implements MenuFixtureFa
             ->setDefault('isSystem', function(Options $options): bool {
                 return $this->faker->boolean(80);
             })
-            ->setDefault('createdAt', function(Options $options): \DateTimeImmutable {
-                return \DateTimeImmutable::createFromMutable($this->faker->dateTime());
-            })
-            ->setDefault('updatedAt', function(Options $options): \DateTimeImmutable {
-                return \DateTimeImmutable::createFromMutable($this->faker->dateTime());
-            })
             ->setDefault('code', function(Options $options): string {
                 return $this->slugGenerator->generate($this->faker->sentence(2, true));
             })
+            ->setDefault('items', [])
         ;
     }
 }
