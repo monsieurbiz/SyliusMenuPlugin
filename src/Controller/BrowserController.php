896b6fe90c384a775c0c43710a596ce4b0c270c3
Maxime Huran <m.huran@monsieurbiz.com>
2024-04-08 18:34:08 +0200

    Add routes and controller for browser

---
+++ src/Controller/BrowserController.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Menu plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusMenuPlugin\Controller;
+
+use MonsieurBiz\SyliusMenuPlugin\Provider\BrowsableObjectProviderInterface;
+use MonsieurBiz\SyliusMenuPlugin\Provider\UrlProviderInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class BrowserController extends AbstractController
+{
+    public function __construct(
+        private BrowsableObjectProviderInterface $browsableObjectProvider
+    ) {
+    }
+
+    public function listAction(
+        Request $request,
+    ): ?Response {
+        $inputName = (string) $request->query->get('inputName', '');
+        $inputValue = (string) $request->query->get('inputValue', '');
+        $locale = (string) $request->query->get('locale', '');
+
+        return $this->render('@MonsieurBizSyliusMenuPlugin/Admin/Browser/_modal.html.twig', [
+            'urlProviders' => $this->browsableObjectProvider->getUrlProviders(),
+            'inputName' => $inputName,
+            'inputValue' => $inputValue,
+            'locale' => $locale,
+        ]);
+    }
+
+    public function listItemsAction(
+        Request $request,
+    ): ?Response {
+        $providerCode = (string) $request->query->get('providerCode', '');
+        $inputName = (string) $request->query->get('inputName', '');
+        $inputValue = (string) $request->query->get('inputValue', '');
+        $locale = (string) $request->query->get('locale', '');
+
+        $urlProvider = $this->findProviderByCode($providerCode);
+        if (null === $urlProvider) {
+            return new JsonResponse(['error' => 'URL Provider not found'], 404);
+        }
+
+        return $this->render('@MonsieurBizSyliusMenuPlugin/Admin/Browser/_modal.html.twig', [
+            'urlProvider' => $urlProvider,
+            'inputName' => $inputName,
+            'inputValue' => $inputValue,
+            'locale' => $locale,
+        ]);
+    }
+
+    private function findProviderByCode(string $code): ?UrlProviderInterface
+    {
+        /** @var UrlProviderInterface $urlProvider */
+        foreach ($this->browsableObjectProvider->getUrlProviders() as $urlProvider) {
+            if ($urlProvider->getCode() === $code) {
+                return $urlProvider;
+            }
+        }
+
+        return null;
+    }
+}
